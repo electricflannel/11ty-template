2a9b2097598a0fffc2be3aa76739a5df3aa69bd2
Jon Wright <jonwright.va@gmail.com>
2020-05-17 20:57:45 -0400

    pause

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
     './src/**/*.svg',
   ],
   theme: {
+    fontSize: {
+      sm: '0.833rem',
+      base: '1rem',
+      lg: '1.2rem',
+      xl: '1.44em',
+      '2xl': '1.728rem',
+      '3xl': '2.074rem',
+      '4xl': '2.488rem'
+
+    },
     extend: {},
   },
   variants: {},
